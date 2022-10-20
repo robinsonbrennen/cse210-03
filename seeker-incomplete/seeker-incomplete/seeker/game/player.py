0240d53b16125bea795e88da24029c4095e0c3b3
robinsonbrennen <rob16073@byui.edu>
2022-10-20 12:39:46 -0600

    terminal fix

--- seeker-incomplete/seeker-incomplete/seeker/game/player.py
+++ seeker-incomplete/seeker-incomplete/seeker/game/player.py
@@ -1,4 +1,4 @@
-from game.terminal import Terminal
+from game.terminal_service import Terminal
 from game.jumper import Jumper
 from game.word import Word
 
@@ -8,7 +8,7 @@ class Player:
 
         self._word_guess = Word()
         self._jumper = Jumper()
-        self._terminal = Terminal()
+        self._terminal_service = Terminal()
         
         
     def start_game(self):
@@ -23,7 +23,7 @@ class Player:
         
     def _get_inputs(self):
 
-        guess = self._terminal.read_text("\nGuess a letter [a-z]: ")
+        guess = self._terminal_service.read_text("\nGuess a letter [a-z]: ")
         
         if not self._word_guess.check_guess(guess):
             self._jumper.take_lives()
@@ -37,6 +37,6 @@ class Player:
         self._jumper.draw_jumper()
         
         if not self._jumper.is_alive():
-            self._terminal.write_text("Game Over!")
+            self._terminal_service.write_text("Game Over!")
         elif self._word_guess.is_guessed():
-            self._terminal.write_text("You guessed it! Congrats!")
+            self._terminal_service.write_text("You guessed it! Congrats!")
